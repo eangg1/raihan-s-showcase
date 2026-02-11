8a4f4adb28650770408ada5720cbd037e207e0a1
raihan <maz.raihan@gmail.com>
2026-02-11 19:12:56 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Projects from "./pages/projects";
+import Projects from "./pages/Projects";
 import Films from "./pages/Films";
 import FilmDetail from "./pages/FilmDetail";
 import Music from "./pages/Music";
